e9483fd50ab099c31e3df855dead2f1c8bf6835f
Chad Austin <chad@chadaustin.me>
2017-02-12 13:28:53 -0800

    checkpoint moving stuff into swift

--- Classes/PassDropAppDelegate.swift
+++ Classes/PassDropAppDelegate.swift
@@ -2,6 +2,21 @@ import UIKit
 
 class PassDropAppDelegate2: NSObject, UIApplicationDelegate, UIAlertViewDelegate {
     override init() {
+        super.init()
     }
-    //let dbManager: DBManager
+    
+    @IBOutlet var window: UIWindow?
+    @IBOutlet var navigationController: UINavigationController?
+    @IBOutlet var splitController: MGSplitViewController?
+
+    /*
+    let dbSession: DBSession
+    let prefs: AppPrefs
+    let dbManager: DatabaseManager
+    */
+
+    //let hide: HideViewController
+    @IBOutlet var rootView: RootViewController?
+    
+    //let settingsView: SettingsViewController
 }
