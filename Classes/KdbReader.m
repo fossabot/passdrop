2a04bc95377e4ba9380985e6213cc3466ce4bc5d
Chad Austin <ca@dropbox.com>
2017-02-13 20:57:51 -0800

    SORT THE PASSWORDS! YAY!

--- Classes/KdbReader.m
+++ Classes/KdbReader.m
@@ -52,9 +52,41 @@
 	return retval != kpass_success;
 }
 
+static int compareGroup(const void* lhs_, const void* rhs_) {
+    const kpass_group* lhs = *(const kpass_group**)lhs_;
+    const kpass_group* rhs = *(const kpass_group**)rhs_;
+    if (lhs->name == NULL && rhs->name == NULL) {
+        return 0;
+    } else if (lhs->name == NULL) {
+        return -1;
+    } else if (rhs->name == NULL) {
+        return 1;
+    } else {
+        return strcasecmp(lhs->name, rhs->name);
+    }
+}
+
+static int compareEntry(const void* lhs_, const void* rhs_) {
+    const kpass_entry* lhs = *(const kpass_entry**)lhs_;
+    const kpass_entry* rhs = *(const kpass_entry**)rhs_;
+    if (lhs->title == NULL && rhs->title == NULL) {
+        return 0;
+    } else if (lhs->title == NULL) {
+        return -1;
+    } else if (rhs->title == NULL) {
+        return 1;
+    } else {
+        return strcasecmp(lhs->title, rhs->title);
+    }
+}
+
 - (KdbGroup*)getRootGroupForDatabase:(id<Database>)database {
     [database setKpDatabase:kpassDb];
     [database setPwHash:kpassPw];
+
+    qsort(kpassDb->groups, kpassDb->groups_len, sizeof(kpassDb->groups[0]), compareGroup);
+    qsort(kpassDb->entries, kpassDb->entries_len, sizeof(kpassDb->entries[0]), compareEntry);
+
 	return [[[KdbGroup alloc] initRootGroupWithCount:kpassDb->groups_len subGroups:kpassDb->groups andCount:kpassDb->entries_len groupEntries:kpassDb->entries forDatabase:database] autorelease];
 }
 
